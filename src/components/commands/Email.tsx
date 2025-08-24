2f2fe3091e5bc8beebbb58a641750714470ff3f1
preethamgshiva <preethamgshiva2004@gmail.com>
2025-08-24 20:07:05 +0530

    Fix Deployment errors

--- src/components/commands/Email.tsx
+++ src/components/commands/Email.tsx
@@ -10,7 +10,7 @@ const Email: React.FC = () => {
   const currentCommand = _.split(history[0], " ");
 
   if (rerender && currentCommand[0] === "email" && currentCommand.length <= 1) {
-    window.open("mailto:" + "contact@satnaing.dev", "_self");
+    window.open("mailto:" + "preethamgshiva2004@gmail.com", "_self");
   }
 
   return (
--- src/components/commands/Welcome.tsx
+++ src/components/commands/Welcome.tsx
@@ -40,7 +40,7 @@ const Welcome: React.FC = () => {
         <Seperator>----</Seperator>
         <div>
           This project's source code can be found in this project's{" "}
-          <Link href="https://github.com/satnaing/terminal-portfolio">
+          <Link href="https://github.com/preethamgshiva/terminal-portfolio">
             GitHub repo
           </Link>
           .
